8c333abb98950791eabac894fedf1aa48da09bf0
d78mdd <d78mdd@gmail.com>
2021-04-03 14:12:27 +0200

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,21 @@
+
+public class Main {
+  public static void main (String[]args) {
+
+    Person per1 = new Person();
+    System.out.println (per1);
+    
+    Person per2 = per1;
+    System.out.println (per1);
+    
+    /*int var2 = var1;
+    var2 = 15;
+
+    System.out.println (var1);
+    */
+  }
+  
+}
+
+
+
