a3ae0ce3b75d76cfcd8eccba6ec4c8400fbf3583
d78mdd <d78mdd@gmail.com>
2021-04-03 14:55:16 +0200

    try without "new" keyword

---
+++ 3/Main.java
@@ -0,0 +1,18 @@
+
+public class Main
+{
+	public static void main(String[] args) {
+	  
+	  Airplane airbus1 = new Airplane();
+	  
+	  System.out.println(airbus1);
+	  
+	  Airplane airbus2;
+	  
+	  /*System.out.println(airbus2);
+	  we get error when we try to access it
+	  */
+	   
+	}
+}
+
