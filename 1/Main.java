94e1633f280ae68e86767980f7bde332706a4c44
d78mdd <d78mdd@gmail.com>
2021-04-03 14:29:46 +0200

    Update Main.java

--- 1/Main.java
+++ 1/Main.java
@@ -3,10 +3,16 @@ public class Main {
   public static void main (String[]args) {
 
     Person per1 = new Person();
-    System.out.println (per1);
+    System.out.println ("per1\t" + per1);
     
     Person per2 = per1;
-    System.out.println (per1);
+    System.out.println ("per2=per1" + per2);
+    
+    System.out.println ("nulling per2");
+    per2 = null;
+    
+    System.out.println("per1: " + per1);
+    System.out.println("per2: " + per2);
     
     /*int var2 = var1;
     var2 = 15;
